2403aa31b37aeedfb2c0427843c5dfd261499ae9
HongSh1991 <hjmrezl@outlook.com>
2018-02-16 23:19:08 +0800

    modify linearregessor

--- Chapter01/03.py
+++ Chapter01/03.py
@@ -1,10 +1,12 @@
 # -*-coding:utf8-*-
-import sys
+# import sys
 import numpy as np
+import cPickle as pickle
 import matplotlib.pyplot as plt
+import sklearn.metrics as sm
 from sklearn import linear_model
 
-filename = sys.argv[1]
+filename = "data_singlevar.txt"
 X = []
 Y = []
 with open(filename, 'r') as f:
@@ -27,9 +29,26 @@ Y_test = np.array(Y[num_training:])
 linear_regressor = linear_model.LinearRegression()
 linear_regressor.fit(X_train, Y_train)
 
-Y_train_pred = linear_regressor.predict(X_train)
+# 保存训练数据训练的模型
+output_model_file = 'save_model.pkl'
+with open(output_model_file, 'w') as f:
+	pickle.dump(linear_regressor, f)
+
+with open(output_model_file, 'r') as f1:
+	model_linregr = pickle.load(f1)
+
+y_test_pred_new = model_linregr.predict(X_test)
+print("\nNew mean absolute error = ", round(sm.mean_absolute_error(Y_test, y_test_pred_new), 2))
+
+"""Y_train_pred = linear_regressor.predict(X_train)
 plt.figure()
 plt.scatter(X_train, Y_train, color='green')
 plt.plot(X_train, Y_train_pred, color='black', linewidth=4)
-plt.title('Training data')
+plt.title('Training data')"""
+
+y_test_pred = linear_regressor.predict(X_test)
+
+plt.scatter(X_test, Y_test, color='green')
+plt.plot(X_test, y_test_pred_new, color='black', linewidth=4)
+plt.title('Test data')
 plt.show()
